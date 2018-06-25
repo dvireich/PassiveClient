ffaddc2b184711871600cad4adeac2524a31786d
dvireich <38475641+dvireich@users.noreply.github.com>
2018-06-26 00:18:33 +0300

    Fixed bugs after added the Ui

--- PassiveClientUi/LoginFrom.cs
+++ PassiveClientUi/LoginFrom.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,23 +30,45 @@ namespace PassiveClientUi
         {
             try
             {
-                this.Visible = false;
+                Cursor.Current = Cursors.WaitCursor;
                 if (!PassiveClient.Program.Authenticate(UserNameTextBox.Text, PasswordTextBox.Text))
                 {
                     MessageBox.Show("User Name or Password are incorrent", "Sign in", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    this.Visible = true;
                     return;
                 }
-                PassiveClient.Program.Main(new string[]
+
+                var messageForSuccess = string.Concat("Successfully Signed In!",
+                                                      Environment.NewLine,
+                                                      "Wating in background for Active user to sign in...");
+
+                MessageBox.Show(messageForSuccess, "Sign in", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Visible = false;
+                Cursor.Current = Cursors.Default;
+
+                try
                 {
-                    "hidden=True",
-                    "morethanoneclinet=false",
-                });
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        UseShellExecute = false,
+                        FileName = "PassiveClient.exe",
+                        Arguments = string.Join(" ", new string[] {
+                                                                    "hidden=true",
+                                                                    "morethanoneclinet=false",
+                                                                     string.Format("username={0}",UserNameTextBox.Text),
+                                                                     string.Format("password={0}",PasswordTextBox.Text)}),
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        CreateNoWindow = true
+                    });
+                }
+                finally
+                {
+                    Environment.Exit(0);
+                }
             }
             catch(Exception e)
             {
+                Cursor.Current = Cursors.Default;
                 MessageBox.Show(e.Message , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                this.Visible = true;
             }
             
         }
