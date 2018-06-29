6d3afb66447df44de0f512a7d76bd93b81116e17
dvireich <38475641+dvireich@users.noreply.github.com>
2018-06-29 17:15:05 +0300

    Enable the DeadlockDetectionPolicy

--- PassiveClient/GlobalAspects.cs
+++ PassiveClient/GlobalAspects.cs
@@ -3,4 +3,4 @@ using PostSharp.Extensibility;
 using PostSharp.Patterns.Threading;
 // This file contains registration of aspects that are applied to several classes of this project.
 [assembly: Log(AttributeTargetTypeAttributes=MulticastAttributes.Private | MulticastAttributes.Protected | MulticastAttributes.Internal | MulticastAttributes.Public, AttributeTargetMemberAttributes=MulticastAttributes.Private | MulticastAttributes.Protected | MulticastAttributes.Internal | MulticastAttributes.Public)]
-//[assembly: DeadlockDetectionPolicy]
+[assembly: DeadlockDetectionPolicy]
